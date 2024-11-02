2d469d53c62cab4bccadd70302d3d4fca6aeeaf1
Sophia <sophiadike@gmail.com>
2024-11-02 02:46:28 +0100

    Assignment done.

---
+++ user_input.py
@@ -0,0 +1,6 @@
+name = input("What is your name?\n")
+age = input("How old are you?\n")
+location = input("What is your location?\n")
+
+print(f"Hello {name}!, you are {age}, you live in {location}. Bye.")
+print("See you later.")
